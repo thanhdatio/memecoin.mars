0f44220646e629d7bebf12b6d739f880e5d8986e
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2024-03-30 16:36:11 -0300

    30/03/2024

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 /*MENU MOBILE */
-const btnMobile = document.querySelector('button.btn-mobile-menu')
-function toggleMenu() {
-  const navMobile = document.querySelector('header nav')
-  btnMobile.classList.toggle('active')
-  navMobile.classList.toggle('active')
-}
-btnMobile.addEventListener('click', toggleMenu)
+// const btnMobile = document.querySelector('button.btn-mobile-menu')
+// function toggleMenu() {
+//   const navMobile = document.querySelector('header nav')
+//   btnMobile.classList.toggle('active')
+//   navMobile.classList.toggle('active')
+// }
+// btnMobile.addEventListener('click', toggleMenu)
 /*MENU MOBILE */
 
 /*SWIPER */
