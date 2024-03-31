fbde211caa710892a07dfd1a26f4313dd73f6fee
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2024-03-31 20:42:40 -0300

    31/03/2024

--- js/script.js
+++ js/script.js
@@ -19,3 +19,25 @@ const swiperGlobal = new Swiper('.swiper-global .swiper', {
 })
 /*SWIPER */
 AOS.init()
+
+const btnMusicPlay = document.querySelector('.music-box button.play')
+const btnMusicPause = document.querySelector('.music-box button.pause')
+const music = document.querySelector('audio.music')
+
+function initMusic() {
+  if (btnMusicPause && btnMusicPlay) {
+    btnMusicPlay.addEventListener('click', playMusic)
+    btnMusicPause.addEventListener('click', pauseMusic)
+    function playMusic() {
+      btnMusicPlay.classList.remove('active')
+      btnMusicPause.classList.add('active')
+      music.play()
+    }
+    function pauseMusic() {
+      btnMusicPause.classList.remove('active')
+      btnMusicPlay.classList.add('active')
+      music.pause()
+    }
+  }
+}
+initMusic()
